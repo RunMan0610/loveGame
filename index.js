a00de1d2091ffbd8fd83f3a2e5f214e2a86312d2
RunMan0610 <116517697+RunMan0610@users.noreply.github.com>
2022-10-29 23:51:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ loveScore = Math.floor(loveScore) + 1;
 if (loveScore>70){
 	document.write(x +" & "+ y +" Your wave lenth is Strong: Your Current Love Wave Lenth Is " +loveScore+"%");
 } else {
-	document.write(x +" & "+ y +" Your wave lenth is week at the moment- Avoid Aurguments: Your Current Love Wave Lenth Is " +loveScore+"%");
+	document.write(x +" & "+ y +" Your wave lenth is weak at the moment- Avoid Aurguments: Your Current Love Wave Lenth Is " +loveScore+"%");
 
 	}
 
