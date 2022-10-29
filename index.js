92beebf7764060660c5a1727ae95e2822b9fb7f8
RunMan0610 <116517697+RunMan0610@users.noreply.github.com>
2022-10-29 21:43:53 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+
+	
+var x = prompt("what is your name?");
+var y = prompt("what is crush name");
+
+var loveScore = Math.random() * 100;
+loveScore = Math.floor(loveScore) + 1;
+
+
+if (loveScore>70){
+	document.write(x +" & "+ y +" Your wave lenth is Strong: Your Current Love Wave Lenth Is " +loveScore+"%");
+} else {
+	document.write(x +" & "+ y +" Your wave lenth is week at the moment- Avoid Aurguments: Your Current Love Wave Lenth Is " +loveScore+"%");
+
+	}
+
+
+
+
+	
